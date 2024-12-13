c64416cc6d948147aeec0c6a5e74f18e17208678
Srinivasan V <59498228+SrinivasanSE@users.noreply.github.com>
2024-12-13 16:49:40 +0530

    Create largest_three_numbers.js

---
+++ largest_three_numbers.js
@@ -0,0 +1,31 @@
+const getThreeLargest = (arr) => {
+        let n = arr.length
+        let m1 = -1,m2 = -1,m3 = -1
+        for(let i = 0; i <n; i++ ) {
+            //console.log(arr[i], m1, m2, m3)
+            if(m1 < arr[i]) {
+                m3 = m2
+                m2 = m1
+                m1 = arr[i]
+                
+            } else if(arr[i] > m2 && m1 > arr[i] ) {
+                m3 = m2
+                m2 = arr[i]
+            } else if(arr[i] > m3 && m2 > arr[i] && m1 > arr[i]) {
+                m3 = arr[i]
+            }
+        }
+        
+        let res = [m1]
+        if (m2 > 0) {
+            res.push(m2)
+        }
+        if (m3 > 0) {
+            res.push(m3)
+        }
+        return res
+}
+
+let arr = [12, 13, 1, 10, 34, 1];
+let res = getThreelargest(arr);
+console.log(res.join(' '));
