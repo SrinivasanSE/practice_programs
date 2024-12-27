cc38a15e990a403b95edbceb21c7cc31ae8b1230
Srinivasan V <59498228+SrinivasanSE@users.noreply.github.com>
2024-12-27 21:58:54 +0530

    Update Count pairs with given sum.js

--- Two Pointers/2Sum/Count pairs with given sum.js
+++ Two Pointers/2Sum/Count pairs with given sum.js
@@ -1,5 +1,8 @@
 // https://www.geeksforgeeks.org/count-pairs-with-given-sum/
+// https://www.geeksforgeeks.org/count-pairs-with-given-sum-in-sorted-array/
 
+
+// Hashmap for unsorted arr
 countPairs(arr, target) {
         const n = arr.length
         let count = 0
@@ -17,3 +20,60 @@ countPairs(arr, target) {
         
         return count
     }
+
+// Two pointer for sorted arr
+countPairs(arr, target) {
+        arr.sort((a, b) => a - b)
+        const n = arr.length
+        let l = 0
+        let r = n - 1
+        let res = 0
+        while (l < r) {
+            const sum = arr[l] + arr[r]
+            
+            if (sum < target) {
+                l++
+            } else if (sum > target) {
+                r--
+            } else {
+                let c1 = 0, c2 = 0
+                const left = arr[l]
+                const right = arr[r]
+                while (l <= r && arr[l] === left) {
+                    c1++
+                    l++
+                }
+                
+                while (l <= r && arr[r] === right) {
+                    c2++
+                    r--
+                }
+                
+                if (left === right) {
+                    res += c1*(c1 - 1)/2   
+                } else {
+                    res += c1*c2
+                }
+            }
+        }
+        
+        return res
+    }
+
+
+/*
+If the elements are the same (ele1 === ele2):
+
+Use the combination formula 
+Count of pairs   = cnt1*(cnt1−1)/2
+
+​where cnt1 is the frequency of ele1.
+This formula calculates how many ways you can choose 2 elements from cnt1.
+
+
+If the elements are different (ele1 !== ele2):
+
+The number of pairs is simply cnt1*cnt2
+as you pair every occurrence of ele1 with every occurrence of ele2.
+
+*/
