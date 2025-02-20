c8d1ed0b37005b292e0a087bf126195e828cbbf7
Srinivasan V <59498228+SrinivasanSE@users.noreply.github.com>
2025-02-20 20:15:22 +0530

    Create Remove duplicates from an unsorted linked list.js

---
+++ LinkedList/Single/Remove duplicates from an unsorted linked list.js
@@ -0,0 +1,23 @@
+// https://www.geeksforgeeks.org/remove-duplicates-from-an-unsorted-linked-list/
+
+class Solution {
+    // Function to remove duplicates from unsorted linked list.
+    removeDuplicates(head) {
+        // your code here
+        const set = new Set()
+        
+        let curr = head
+        let prev = null
+        while(curr) {
+            if (set.has(curr.data)) {
+                prev.next = curr.next
+            } else {
+                prev = curr
+                set.add(curr.data)
+            }
+            curr = curr.next
+        }
+        
+        return head
+    }
+}
