84ab5f4099b70bb2673d0ca4f1679ed64327824f
Srinivasan V <59498228+SrinivasanSE@users.noreply.github.com>
2025-02-23 12:10:33 +0530

    Create Pairwise swap elements of a linked list.js

---
+++ LinkedList/Single/Pairwise swap elements of a linked list.js
@@ -0,0 +1,52 @@
+// https://www.geeksforgeeks.org/pairwise-swap-elements-of-a-given-linked-list/
+// https://www.geeksforgeeks.org/pairwise-swap-elements-of-a-given-linked-list-by-changing-links/
+
+
+class Solution {
+    pairWiseSwap(head) {
+        // your code here
+        if(!head || !head.next) {
+            return head
+        }
+        
+        let prev = head
+        let curr = head.next
+        
+        let newHead = curr
+        
+        while(1) {
+            let next = curr.next
+            curr.next = prev
+            
+            if(!next || !next.next) {
+                prev.next = next
+                break
+            }
+            
+            prev.next = next.next
+            prev = next
+            curr = prev.next
+        }
+        
+        return newHead
+        
+    }
+}
+
+class Solution {
+    pairWiseSwap(head) {
+        // your code here
+        let curr = head;
+
+    while (curr !== null && curr.next !== null) {
+        
+        [curr.data, curr.next.data] = [curr.next.data, curr.data];
+
+        // Move to the next pair
+        curr = curr.next.next;
+    }
+    
+    return head
+        
+    }
+}
