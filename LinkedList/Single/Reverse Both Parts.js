4edae879e842654374aafb8360bf5a24e51e70f6
Srinivasan V <59498228+SrinivasanSE@users.noreply.github.com>
2025-03-02 11:59:56 +0530

    Create Reverse Both Parts.js

---
+++ LinkedList/Single/Reverse Both Parts.js
@@ -0,0 +1,50 @@
+// https://www.geeksforgeeks.org/problems/reverse-both-parts--170647/1?page=2&category=Linked%20List&difficulty=Easy&sortBy=submissions
+
+
+class Solution {
+    /**
+     * @param {Node} head
+     * @param {number} k
+     * @returns {Node}
+     */
+     
+    _reverse(head) {
+        if (!head) {
+            return head
+        }
+        let curr = head
+        let prev = null
+        while(curr) {
+            let nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+        }
+        
+        return prev
+    }
+    reverse(head, k) {
+        // code here
+        let i = 1
+        let curr = head
+        while(i < k) {
+            curr = curr.next
+            i++
+        }
+        
+        let temp = curr.next
+        curr.next = null
+        head = this._reverse(head)
+        //console.log(head1)
+        temp = this._reverse(temp)
+        curr = head
+        while(curr.next) {
+            curr = curr.next
+        }
+        curr.next = temp
+        return head
+        
+        
+    
+    }
+}
