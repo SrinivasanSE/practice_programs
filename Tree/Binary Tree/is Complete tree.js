1cb02a130d871b1f69acecb0b57f8344a3dfe5e5
Srinivasan V <59498228+SrinivasanSE@users.noreply.github.com>
2025-04-27 12:26:08 +0530

    heap

---
+++ Tree/Binary Tree/is Complete tree.js
@@ -0,0 +1,83 @@
+// https://www.geeksforgeeks.org/check-if-a-given-binary-tree-is-complete-tree-or-not/
+
+
+class Solution {
+
+    // Function to check whether a binary tree is balanced or not.
+    isComplete(root) {
+        // your code here
+        
+        if (root == null) {
+            return true
+        }
+        
+        let q = [root]
+        let isNullNode = false
+        while (q.length > 0) {
+            let curr = q.shift()
+            
+            if (curr == null) {
+                isNullNode = true
+            } else {
+                
+                if(isNullNode) {
+                    return false
+                }
+                
+                q.push(curr.left)
+                q.push(curr.right)
+            }
+        }
+        
+        return true
+    }
+}
+
+
+
+// User function Template for javascript
+
+// User function Template for javascript
+
+/**
+ * @param {Node} root
+ * @returns {boolean}
+ */
+
+/*
+class Node{
+    constructor(data){
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+*/
+
+class Solution {
+
+    // Function to check whether a binary tree is balanced or not.
+    countNodes(root) {
+        if (root == null) {
+            return 0
+        }
+        
+        return 1 + this.countNodes(root.left) + this.countNodes(root.right)
+    }
+    
+    _isComplete(node, index, count) {
+        if (node === null) {
+            return true
+        }
+        
+        if (index >= count) {
+            return false
+        }
+        
+        return this._isComplete(node.left, 2*index + 1, count) && this._isComplete(node.right, 2*index + 2, count)
+    }
+    isComplete(root) {
+        // your code here
+        return this._isComplete(root, 0, this.countNodes(root))
+    }
+}
