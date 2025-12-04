8faa91c8e0f1b5dd1d96d579a539e7175a5b895d
Srinivasan V <59498228+SrinivasanSE@users.noreply.github.com>
2025-12-04 23:23:08 +0530

    revision

--- Stack/problems/Medium/907. Sum of Subarray Minimums.js
+++ Stack/problems/Medium/907. Sum of Subarray Minimums.js
@@ -17,7 +17,7 @@ const findNSE = (arr, n) => {
         }
 
         res[i] = stk.length === 0 ? n : stk[stk.length - 1]
-        stk.push(i)
+        stk.push(i) // we push the index
     }
 
     return res
@@ -32,7 +32,7 @@ const findPSE = (arr, n) => {
         }
 
         res[i] = stk.length === 0 ? -1 : stk[stk.length - 1]
-        stk.push(i)
+        stk.push(i) // we push the index
     }
 
     return res
@@ -54,4 +54,75 @@ var sumSubarrayMins = function(arr) {
 
     return total
 
+};
+
+
+// Optimised version
+
+/*
+
+Instead of calc for the curr index, we calculate for the popped index, when we pop the index, that means the curr element is the next smallest element for the popped index
+The stack is monotonically increasing stack, the previous smallest element for the popped index is the current stack's top.
+
+Stack(it contains index, but let's assume these are values) = [1, 2, 4], curr element = 3.
+
+4 > 3, so for 4, the nse is 3 and pse is 2
+
+
+*/
+
+
+var sumSubarrayMins = function (arr) {
+    let sum = 0
+    const n = arr.length
+
+    // Variables:
+    // left  = how many subarrays extend to the LEFT where arr[curr] is minimum
+    // right = how many subarrays extend to the RIGHT where arr[curr] is minimum
+    // stk   = monotonic INCREASING stack storing indices
+    // pse   = Previous Smaller Element index
+    // nse   = Next Smaller Element index
+    // curr  = index popped from stack (the one we are calculating contribution for)
+    let left, right, stk = [], pse, nse, curr
+
+    for (let i = 0; i <= n; i++) {
+
+        // We loop one extra time (i == n) to flush the stack - some elements will remain in the stack at the end, for these elements, 
+        // there are no next smallest element, if nse was there, these elements would have been popped, so for all them nse will be considered as n
+        // Pop while:
+        //   - we reached the end, OR
+        //   - the current value arr[i] is smaller than the value at stack top.
+        // This means we've found the "next smaller element" for stackTop.
+        while (stk.length > 0 && (i === n || arr[i] < arr[stk[stk.length - 1]])) {
+
+            // curr = index whose contribution we are calculating
+            curr = stk.pop()
+
+            // nse = Next Smaller Element index
+            //      For popped "curr", the next smaller is "i"
+            nse = i
+
+            // pse = Previous Smaller Element index
+            //      Left boundary: previous smaller is new stack top (if exists)
+            //      If no previous smaller → use -1
+            pse = stk.length === 0 ? -1 : stk[stk.length - 1]
+
+            // Number of subarrays to the left where arr[curr] remains minimum
+            left = curr - pse
+
+            // Number of subarrays to the right where arr[curr] remains minimum
+            right = nse - curr
+
+            // Contribution formula:
+            // arr[curr] is the minimum of `left * right` different subarrays.
+            // Add its contribution into sum.
+            sum = (sum + (arr[curr] * left * right) % MOD) % MOD
+        }
+
+        // Push current index into stack
+        // Stack stores indices in increasing order of arr[]
+        stk.push(i)
+    }
+
+    return sum
 };
