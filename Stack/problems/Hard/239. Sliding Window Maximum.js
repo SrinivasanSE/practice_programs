ddfd3c5671e4d2f5dfd3bcc566eda3254e5f88cf
Srinivasan V <59498228+SrinivasanSE@users.noreply.github.com>
2026-02-22 00:07:06 +0530

    revision

--- Stack/problems/Hard/239. Sliding Window Maximum.js
+++ Stack/problems/Hard/239. Sliding Window Maximum.js
@@ -8,51 +8,55 @@ O(2n) & O(k) + O(n - k)
 */
 
 var maxSlidingWindow = function (nums, k) {
-    let output = [], dequeue = []
-    const n = nums.length
-    for (let i = 0; i < n; i++) {
-        if (dequeue.length > 0 && dequeue[0] <= i - k) { // pop when the max element is not within the current window
-            dequeue.shift()
-        }
-
-        // for sliding window min, the condition should be  >=, similar to PSE
-        while (dequeue.length > 0 && nums[dequeue[dequeue.length - 1]] <= nums[i]) { // pop the elements which are less than the curr element
-            dequeue.pop()
-        }
-
-        dequeue.push(i)
-        if (i >= k - 1) { // when we reach the k size, push the front element from the dequeue which holds the max
-            output.push(nums[dequeue[0]])
-        }
+  let output = [],
+    dequeue = [];
+  const n = nums.length;
+  for (let i = 0; i < n; i++) {
+    if (dequeue.length > 0 && dequeue[0] <= i - k) {
+      // pop when the max element is not within the current window
+      dequeue.shift();
     }
 
-    return output
+    // for sliding window min, the condition should be  >=, similar to PSE
+    while (dequeue.length > 0 && nums[dequeue[dequeue.length - 1]] <= nums[i]) {
+      // pop the elements which are less than the curr element
+      dequeue.pop();
+    }
+
+    dequeue.push(i);
+    if (i >= k - 1) {
+      // when we reach the k size, push the front element from the dequeue which holds the max
+      output.push(nums[dequeue[0]]);
+    }
+  }
+
+  return output;
 };
 
 // Sliding window approach
 
-
 var maxSlidingWindow = function (nums, k) {
-    const res = [], dequeue = []
+  const res = [],
+    dequeue = [];
 
-    const n = nums.length
+  const n = nums.length;
 
-    let left = 0, right = 0
+  let left = 0,
+    right = 0;
 
-    while (right < n) {
-        while (dequeue.length > 0 && dequeue[dequeue.length - 1] < nums[right]) {
-            dequeue.pop()
-        }
-        dequeue.push(nums[right])
-        if (right >= k - 1) {
-            res.push(dequeue[0])
-
-            if (dequeue[0] === nums[left]) dequeue.shift()
-            left++
-        }
-        right++
+  while (right < n) {
+    while (dequeue.length > 0 && dequeue[dequeue.length - 1] < nums[right]) { // why < and not <=
+      dequeue.pop();
     }
+    dequeue.push(nums[right]);
+    if (right >= k - 1) {
+      res.push(dequeue[0]);
 
+      if (dequeue[0] === nums[left]) dequeue.shift();
+      left++;
+    }
+    right++;
+  }
 
-    return res
+  return res;
 };
