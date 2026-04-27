44ceb29405030685e8aa6919f0e328e39c1d5a1b
Srinivasan V <59498228+SrinivasanSE@users.noreply.github.com>
2026-04-27 22:01:13 +0530

    revision

--- DP/MCM/132. Palindrome Partitioning II.js
+++ DP/MCM/132. Palindrome Partitioning II.js
@@ -1,6 +1,5 @@
 // https://leetcode.com/problems/palindrome-partitioning-ii/description/
 
-
 /*
 
 Recursion/Memo
@@ -9,57 +8,57 @@ O(n^3) & O(n^2) & O(n)
 
 */
 
-
-
 // Helper function to check if a string is palindrome
 const isPalindrome = (str) => {
-    const n = str.length;
-    if (n === 1) return true; // Single character is always a palindrome
-    let i = 0, j = n - 1;
+  const n = str.length;
+  if (n === 1) return true; // Single character is always a palindrome
+  let i = 0,
+    j = n - 1;
 
-    while (i < j) {
-        if (str[i] != str[j]) return false; // Mismatch → not palindrome
-        i++;
-        j--;
-    }
+  while (i < j) {
+    if (str[i] != str[j]) return false; // Mismatch → not palindrome
+    i++;
+    j--;
+  }
 
-    return true; // All characters matched
-}
-
-var minCut = function (s) {
-    const n = s.length;
-
-    // dp[i] will store min cuts needed for substring s[i..n-1]
-    const dp = new Array(n).fill(-1);
-
-    // Recursive function starting from index i
-    const f = (i) => {
-        if (i === n) return 0; // Reached end → no more cuts needed
-
-        if (dp[i] != -1) return dp[i]; // Return memoized result
-
-        let temp = ""; // To build substring from i to j
-        let count, min = Number.MAX_SAFE_INTEGER;
-
-        for (let j = i; j < n; j++) {
-            temp += s[j]; // Extend substring
-
-            if (isPalindrome(temp)) { // If current substring is palindrome
-                if (j === n - 1) {
-                    count = 0; // No cut needed if palindrome reaches end
-                } else {
-                    count = 1 + f(j + 1); // 1 cut + min cuts for remaining string
-                }
-                min = Math.min(min, count); // Take minimum among all options
-            }
-        }
-
-        return dp[i] = min; // Memoize and return
-    }
-
-    return f(0); // Start from index 0
+  return true; // All characters matched
 };
 
+var minCut = function (s) {
+  const n = s.length;
+
+  // dp[i] will store min cuts needed for substring s[i..n-1]
+  const dp = new Array(n).fill(-1);
+
+  // Recursive function starting from index i
+  const f = (i) => {
+    if (i === n) return 0; // Reached end → no more cuts needed
+
+    if (dp[i] != -1) return dp[i]; // Return memoized result
+
+    let temp = ""; // To build substring from i to j
+    let count,
+      min = Number.MAX_SAFE_INTEGER;
+
+    for (let j = i; j < n; j++) {
+      temp += s[j]; // Extend substring
+
+      if (isPalindrome(temp)) {
+        // If current substring is palindrome
+        if (j === n - 1) {
+          count = 0; // No cut needed if palindrome reaches end
+        } else {
+          count = 1 + f(j + 1); // 1 cut + min cuts for remaining string
+        }
+        min = Math.min(min, count); // Take minimum among all options
+      }
+    }
+
+    return (dp[i] = min); // Memoize and return
+  };
+
+  return f(0); // Start from index 0
+};
 
 /*
 
@@ -69,37 +68,80 @@ O(n^2) & O(n)
 
 */
 
+const _isPalindrome = (l, r, s) => {
+  while (l < r) {
+    if (s[l] != s[r]) return false;
+    l++;
+    r--;
+  }
+
+  return true;
+};
+
 var minCut = function (s) {
-    const n = s.length;
+  const n = s.length;
 
-    // dp[i] stores min cuts needed for substring s[i..n-1]
-    const dp = new Array(n).fill(0);
+  const dp = new Array(n).fill(0);
 
-    // Precompute palindrome substrings
-    const isPalindrome = Array.from({length: n}, () => new Array(n).fill(false));
-    for (let i = n - 1; i >= 0; i--) {
-        for (let j = i; j < n; j++) {
-            // A substring s[i..j] is palindrome if:
-            // - Ends match AND
-            // - Substring length < 2 (single char) OR middle substring is palindrome (if s[i + 1, j - 1] is a palindrome, then s[i, j] will be palindrome if end chars match)
-            if (s[i] === s[j] && (j - i <= 1 || isPalindrome[i + 1][j - 1])) { // when i is 5, we would have already computed the value of i = 6
-                isPalindrome[i][j] = true;
-            }
+  for (let i = n - 1; i >= 0; i--) {
+    let temp = "",
+      min = Number.MAX_SAFE_INTEGER,
+      cuts;
+    for (let j = i; j < n; j++) {
+      temp += s[j];
+      if (_isPalindrome(i, j, s)) {
+        if (j == n - 1) {
+          cuts = 0;
+        } else {
+          cuts = 1 + dp[j + 1];
         }
+        min = Math.min(min, cuts);
+      }
     }
 
-    // Compute minimum cuts from right to left
-    for (let i = n - 1; i >= 0; i--) {
-        let min = Number.MAX_SAFE_INTEGER;
-        for (let j = i; j < n; j++) {
-            if (isPalindrome[i][j]) { // If substring s[i..j] is palindrome
-                // If palindrome reaches end → 0 cuts, else 1 cut + dp[j+1]
-                let count = (j === n - 1) ? 0 : 1 + dp[j + 1];
-                min = Math.min(min, count); // Take minimum
-            }
-        }
-        dp[i] = min; // Store min cuts for s[i..n-1]
-    }
+    dp[i] = min;
+  }
 
-    return dp[0]; // Minimum cuts for the whole string
+  return dp[0];
+};
+
+// Optimal
+
+var minCut = function (s) {
+  const n = s.length;
+
+  // dp[i] stores min cuts needed for substring s[i..n-1]
+  const dp = new Array(n).fill(0);
+
+  // Precompute palindrome substrings
+  const isPalindrome = Array.from({ length: n }, () =>
+    new Array(n).fill(false),
+  );
+  for (let i = n - 1; i >= 0; i--) {
+    for (let j = i; j < n; j++) {
+      // A substring s[i..j] is palindrome if:
+      // - Ends match AND
+      // - Substring length < 2 (single char) OR middle substring is palindrome (if s[i + 1, j - 1] is a palindrome, then s[i, j] will be palindrome if end chars match)
+      if (s[i] === s[j] && (j - i <= 1 || isPalindrome[i + 1][j - 1])) {
+        // when i is 5, we would have already computed the value of i = 6
+        isPalindrome[i][j] = true;
+      }
+    }
+  }
+
+  // Compute minimum cuts from right to left
+  for (let i = n - 1; i >= 0; i--) {
+    let min = Number.MAX_SAFE_INTEGER;
+    for (let j = i; j < n; j++) {
+      if (isPalindrome[i][j]) {
+        // If substring s[i..j] is palindrome
+        // If palindrome reaches end → 0 cuts, else 1 cut + dp[j+1]
+        let count = j === n - 1 ? 0 : 1 + dp[j + 1];
+        min = Math.min(min, count); // Take minimum
+      }
+    }
+    dp[i] = min; // Store min cuts for s[i..n-1]
+  }
+
+  return dp[0]; // Minimum cuts for the whole string
 };
--- DP/MCM/README.md
+++ DP/MCM/README.md
@@ -1,48 +1,69 @@
 # MCM
 
-1) Matrix chain multiplication
+1. Matrix chain multiplication
 
 - The func takes 2 params, i and j
 - We start the recursion from 1 and n - 1.
 - Base condition:
-    if i == j return 0
-- We need to run a for loop from i to j and find the min steps. 
-    Steps = arr[i - 1] * arr[k] * arr[j] + f(i, k) + f(k + 1, j)
+  if i == j return 0
+- We need to run a for loop from i to j and find the min steps.
+  Steps = arr[i - 1] _ arr[k] _ arr[j] + f(i, k) + f(k + 1, j)
 - For tabulation, we run the for loop from i = n - 1 to 1 and j from i + 1 to n.
 
-
-2) Minimum Cost to Cut a Stick
+2. Minimum Cost to Cut a Stick
 
 - The func takes 2 params, i and j
 - We add 0 at the start and given n at the end of the arr and sort the arr
 - We start the recursion from 1 and c ( length of the given arr).
 - Base condition:
-    if i > j return 0
-- We need to run a for loop from i to j (inclusive) and find the min cuts. 
-    Cuts = cuts[j + 1] - cuts[i - 1] + f(i, k - 1) + f(k + 1, j)
+  if i > j return 0
+- We need to run a for loop from i to j (inclusive) and find the min cuts.
+  Cuts = cuts[j + 1] - cuts[i - 1] + f(i, k - 1) + f(k + 1, j)
 - For tabulation, we run the for loop from i = c to 1 and j from i to c.
 
-3) Burst Balloons
+3. Burst Balloons
 
 - The func takes 2 params, i and j
 - We add 0 at the start and given n at the end of the arr.
 - We start the recursion from 1 and n ( length of the given arr).
 - Base condition:
-    if i > j return 0
-- We need to run a for loop from i to j (inclusive) and find the max coins. 
-    Coins = nums[i - 1] * nums[k] * nums[j + 1] + f(i, k - 1) + f(k + 1, j)
+  if i > j return 0
+- We need to run a for loop from i to j (inclusive) and find the max coins.
+  Coins = nums[i - 1] _ nums[k] _ nums[j + 1] + f(i, k - 1) + f(k + 1, j)
 - For tabulation, we run the for loop from i = c to 1 and j from i to c.
 
+## 3DP
 
-4) Boolean Parenthesization (# 3DP problem)
+4. Boolean Parenthesization
 
 - The func takes 3 params, i and j and isTrue
 - We start the recursion from 0 and n - 1 and 1.
 - Base condition:
-    if i > j return 0
-    if i == j
-        if (isTrue) return s[i] == 'T' ? 1 : 0
-        return s[i] == 'F' ? 1 : 0
+  if i > j return 0
+  if i == j
+  if (isTrue) return s[i] == 'T' ? 1 : 0
+  return s[i] == 'F' ? 1 : 0
 - We need to run a for loop from i + 1 to j and increment it by 2 to only partition at operator
 - We will find lT, lF, rT, rF and find the no of ways if the s[i] is & or | or ^
 - For tabulation, we run the for loop from 3 to n (inclusive) and 0 to i + len - 1 < n and consider j as i + len - 1
+
+## Front partition
+
+5. Palindrome Partitioning
+
+- The func takes 1 params, i
+- We start the recursion from 0
+- Base condition
+  if i == n return 0
+- We need to run a for loop from i to n and check whether the current substring is a palindrome, if j == n - 1, then the count is 0, else count = 1 + f(j + 1) and
+  take the min
+- Optimal approach is creating a palindrome mapping and using that we can find whether a substring is palindrome or not easily.
+
+6. Partition Array for Maximum Sum
+
+- The func takes 1 params, i
+- We start the recursion from 0
+- Base condition
+  if i == n return 0
+- We need to run a for loop from i to Math.min(i + k, n) and find the maxNum in the curr subrr, find the sum which is (j - i + 1) \* maxNum + f(j + 1), track
+  the maxSum
